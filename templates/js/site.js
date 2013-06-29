dafcd848b2080e3f6aec415cbef555c78811f3ba
Brandon Sandrowicz <brandon@sandrowicz.org>
2013-06-28 23:16:09 -0400

    Parring down the mapping controls

--- templates/js/site.js
+++ templates/js/site.js
@@ -3,6 +3,11 @@ Site = {
         zoom: 11,
         center: new google.maps.LatLng(43.726, -79.385),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
+        // streetViewControl: false,
+        panControl: false,
+        zoomControlOptions: {
+            style: google.maps.ZoomControlStyle.SMALL,
+        },
     },
 
     'initialize': function() {
