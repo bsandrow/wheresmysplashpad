7516e8152ad4aeba84421c14ffad2603e2872d23
Brandon Sandrowicz <brandon@sandrowicz.org>
2013-07-06 11:31:31 -0400

    Refactor of site.js
    - Refactoring site.js
    - Updating address search to center and zoom on the address.
    - Updating map initialization to bound all of the parks on the map.

--- templates/js/site.js
+++ templates/js/site.js
@@ -1,147 +1,192 @@
-Site = {
-    'map_options': {
-        zoom: 11,
-        center: new google.maps.LatLng(43.726, -79.385),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        panControl: false,
-        zoomControlOptions: {
-            style: google.maps.ZoomControlStyle.SMALL,
-        },
-    },
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
 
-    'initialize': function() {
-        this.error_div = $('#error');
-        this.hideError();
+var App = (function($) {
 
-        this.map_div = $('#map');
-        this.map = new google.maps.Map(this.map_div[0], this.map_options);
-        this.fetchParkData();
+    var info_template = (
+        "<div class=\"infowin_content\">" +
+        "  <h1>{0}</h1>" +
+        "  <a href=\"{1}\" title=\"{2} Page\"> Park Page on Toronto.ca </a>" +
+        "</div>"
+    );
 
-        this.geocoder = new google.maps.Geocoder();
-        this.address_box = $('#address');
-        this.address_btn = $('#address-button');
+    // ====
 
-        this.address_btn.click(function(event) {
-            event.preventDefault();
-            Site.searchForAddress();
-        });
+    var expand_button = {
+        init: function() { this.element = $("#excol-button"); },
+        set_expanded: function() { this.element.attr('class', 'icon-minus-sign'); },
+        set_collapsed: function() { this.element.attr('class', 'icon-plus-sign'); },
+    };
 
-        this.expandable.init();
-    },
+    var expandable_click_handler = function(event) {
+        event.preventDefault;
+        App.expandable_content.toggle();
+    };
 
-    'expandable': {
-        'button': {
-            init: function() {
-                this.element = $("#excol-button");
-            },
-            set_expanded: function() {
-                this.element.attr('class', 'icon-minus-sign');
-            },
-            set_collapsed: function() {
-                this.element.attr('class', 'icon-plus-sign');
-            },
-        },
-        'init': function() {
+    var expandable_content = {
+        init: function() {
             this.div = $("#expandable");
             this.button.init();
             this.collapse();
-            this.button.element.click(this.clickHandler);
+            this.button.element.click(expandable_click_handler);
         },
-        'clickHandler': function(event) {
-            event.preventDefault;
-            Site.expandable.toggle();
-        },
-        'expand': function() {
+        button: expand_button,
+        expand: function() {
             this.div.show();
             this.button.set_expanded();
             this.state = "expanded";
         },
-        'collapse': function() {
+        collapse: function() {
             this.div.hide();
             this.button.set_collapsed();
             this.state = "collapsed";
         },
-        'toggle': function() {
+        toggle: function() {
             if (this.state == "expanded") {
                 this.collapse();
             } else {
                 this.expand();
             }
         },
-    },
+    };
 
-    'showError': function(errorMessage) {
-        this.error_div.show().html(errorMessage);
-    },
+    // ====
 
-    'hideError': function() {
-        this.error_div.hide();
-    },
-
-    'centerMap': function(position) {
-        this.map.setCenter(position);
-    },
-
-    'searchForAddress': function() {
-        var site = this;
-        site.geocoder.geocode(
-            {
-                address: site.address_box[0].value
+    var fetch_park_data = function() {
+        $.ajax('/splash-pads.json', {
+            'dataType': 'json',
+            'error': function() {
+                site.show_error("<strong>Error!</strong> Unable to fetch park data.");
             },
-            function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                    var loc = results[0].geometry.location;
-                    var latlng = new google.maps.LatLng(loc.lat(), loc.lng());
-                    site.centerMap(latlng);
-                    var marker = new google.maps.Marker({
-                        position: latlng,
-                        map: site.map,
-                        icon: {
-                            path: google.maps.SymbolPath.CIRCLE,
-                            scale: 5,
-                        },
-                    });
-                } else {
-                    site.setError("<strong>Error!</strong> Could not find any result.");
+            'success': function(data) {
+                //site.park_data = data;
+                for (var i = 0; i < data.length; i++) {
+                    site.map.add_park(data[i]);
                 }
-            }
-        );
-    },
+            },
+        });
+    };
 
-    'addParkToMap': function(park) {
-        var place = new google.maps.LatLng(park.coords[0], park.coords[1]);
-        var marker = new google.maps.Marker({
-            position: place,
-            map: this.map,
-            title: park.name,
-            scrollWheel: false,
-        });
-        var info_content = '<div class="infowin_content">'+
-            '<h1>' + park.name + '</h1>' +
-            '<a href="' + park.url + '" title="' + park.name +
-            ' Page"> Park Page on Toronto.ca </a>'+
-            '</div>';
-        var infowindow = new google.maps.InfoWindow({
-            content: info_content
-        });
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.open(this.map, marker);
-        });
-    },
 
-    'fetchParkData': function() {
-        var site = this;
-        $.ajax(
-            '/splash-pads.json',
-            {
-                'dataType': 'json',
-                'error': function() { site.showError("<strong>Error!</strong> Unable to fetch park data."); },
-                'success': function(data) {
-                    site.park_data = data;
-                    for (var i = 0; i < data.length; i++) {
-                        site.addParkToMap(data[i]);
+    var map = {
+        options: {
+            zoom: 11,
+            center: new google.maps.LatLng(43.726, -79.385),
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            panControl: false,
+            zoomControlOptions: {
+                style: google.maps.ZoomControlStyle.SMALL,
+            },
+        },
+        initialize: function() {
+            this.div = $("#map")[0];
+            this.map = new google.maps.Map(this.div, this.options);
+            this.bounds = new google.maps.LatLngBounds();
+        },
+        add_park: function(park) {
+            var coordinates = new google.maps.LatLng(park.coords[0], park.coords[1]);
+
+            var marker = new google.maps.Marker({
+                position: coordinates,
+                map: this.map,
+                title: park.name,
+                scrollWheel: false,
+            });
+
+            this.bounds.extend(coordinates);
+            this.map.fitBounds(this.bounds);
+
+            var infowindow = new google.maps.InfoWindow({
+                content: info_template.format(park.name, park.url, park.name),
+            });
+
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.open(App.map.map, marker);
+            });
+        },
+        center_map: function(position) {
+            this.map.setCenter(position);
+        },
+    };
+
+    // ====
+
+    var geocoder = new google.maps.Geocoder();
+    var address_search = {
+        initialize: function() {
+            this.address_box = $('#address');
+            this.address_btn = $('#address-button');
+
+            this.address_btn.click(function(event) {
+                event.preventDefault();
+                address_search.find_address();
+            });
+        },
+        find_address: function() {
+            geocoder.geocode({
+                    address: address_search.address_box[0].value
+                },
+                function (results, status) {
+                    if (status == google.maps.GeocoderStatus.OK) {
+                        var result = results[0];
+
+                        var latlng = new google.maps.LatLng(
+                            result.geometry.location.lat(),
+                            result.geometry.location.lng()
+                        );
+
+                        App.map.center_map(latlng);
+                        App.map.map.setZoom(13);
+
+                        var marker = new google.maps.Marker({
+                            position: latlng,
+                            map: App.map.map,
+                            icon: {
+                                path: google.maps.SymbolPath.CIRCLE,
+                                scale: 5,
+                            },
+                        });
+                    } else {
+                        App.set_error("<strong>Error!</strong> Could not find any result.");
                     }
                 }
-            });
-    },
-};
+            );
+        },
+    };
+
+    // ====
+
+    var site = {
+        initialize: function() {
+            this.error_div = $('#error');
+            this.hide_error();
+
+            this.map = map;
+            this.map.initialize();
+            fetch_park_data();
+
+            this.address_search = address_search;
+            this.address_search.initialize();
+
+            this.expandable_content = expandable_content;
+            this.expandable_content.init();
+        },
+        show_error: function(error_message) {
+            this.error_div.show().html(error_message);
+        },
+        hide_error: function() {
+            this.error_div.hide();
+        },
+    };
+    return site;
+
+})(jQuery);
